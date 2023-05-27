aefa597eafb533879d613390f0352e6590c69296
undef <32277378+undefjs@users.noreply.github.com>
2023-05-28 00:53:56 +0200

    Update index.js
    Added JSDoc
    Added ts-check
    Optimized
    Converted to module

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// @ts-check
 'use strict';
 
-function array_interleave(arr) {
-  const ret = new Array(arr.length);
-  const mid = Math.ceil(arr.length / 2);
+/**
+ * Interleave a number array
+ * @param {number[]} arr
+ * @returns {number[]}
+ * @example
+ * // returns [ 1, 8, 2, 7, 3, 6, 4, 5 ]
+ * array_interleave([ 1, 2, 3, 4, 5, 6, 7, 8 ]);
+ */
+export function array_interleave(arr) {
+  const len = arr.length;
+  const ret = new Array(len);
+  const mid = Math.ceil(len / 2);
   for(let i = 0, n = 0, j = 0; i < mid; i++) {
     ret[j++] = arr[i];
-    if(i !== (n = arr.length - i - 1)) {
+    if(i !== (n = len - i - 1)) {
       ret[j++] = arr[n];
     }
   }
   return ret;
 }
-
-module.exports = array_interleave;
